61aef7b6e6d5b11972e231422e5e40c115348cd1
Chris Nelson <chris.nelson@sap.com>
2026-09-06 09:45:21 -0400

    Wired contacts POST request to db with error handling.

--- app/routes/contacts.py
+++ app/routes/contacts.py
@@ -1,15 +1,42 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from psycopg.errors import ForeignKeyViolation, UniqueViolation
 
+from app.db.connection import get_connection
 from app.schemas import ContactCreate, ContactResponse
 
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
 
-@router.post("", response_model=ContactResponse)
+@router.post("", response_model=ContactResponse, status_code=201)
 def create_contact(contact: ContactCreate):
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO contacts (name, email, company_id)
+                    VALUES (%s, %s, %s)
+                    RETURNING id, company_id, name, email
+                    """,
+                    (contact.name, contact.email, contact.company_id),
+                )
+                new_contact = cursor.fetchone()
+
+    except UniqueViolation:
+        raise HTTPException(
+            status_code=409,
+            detail="Contact email already exists"
+        )
+
+    except ForeignKeyViolation:
+        raise HTTPException(
+            status_code=409,
+            detail="Invalid company_id"
+        )
+
     return {
-        "id":1,
-        "name": contact.name,
-        "email": contact.email,
-        "company_id": contact.company_id,
+        "id": new_contact[0],
+        "company_id": new_contact[1],
+        "name": new_contact[2],
+        "email": new_contact[3],
     }
