5db73a8dd49f0a4e735eb7dd67f767171d363e30
Chris Nelson <chris.nelson@sap.com>
2026-09-08 13:56:16 -0400

    Created and tested API end point that gets all contacts by company

--- app/routes/companies.py
+++ app/routes/companies.py
@@ -1,15 +1,13 @@
-from email.policy import HTTP
-
 from fastapi import APIRouter, HTTPException
 from psycopg.errors import UniqueViolation
 
 from app.db.connection import get_connection
-from app.schemas import CompanyCreate, CompanyResponse
+from app.schemas import CompanyCreate, CompanyResponse, ContactResponse
 
 router = APIRouter(prefix="/companies", tags=["companies"])
 
 
-@router.get("/{company_id}")
+@router.get("/{company_id}", response_model=CompanyResponse)
 def get_company(company_id: int):
     with get_connection() as connection:
         with connection.cursor() as cursor:
@@ -19,17 +17,44 @@ def get_company(company_id: int):
             )
             company = cursor.fetchone()
 
-        if company is None:
-            raise HTTPException(
-                status_code=404,
-                detail="Company not found",
-            )
+    if company is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Company not found",
+        )
 
-        return {
-            "id": company[0],
-            "name": company[1],
-            "industry": company[2]
-        }
+    return {
+        "id": company[0],
+        "name": company[1],
+        "industry": company[2]
+    }
+
+@router.get(
+        "/{company_id}/contacts",
+        response_model=list[ContactResponse],
+)
+def get_contacts_by_company(company_id: int):
+    with get_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT id, company_id, name, email
+                FROM contacts
+                WHERE company_id = %s
+                """,
+                (company_id,),
+            )
+            contacts = cursor.fetchall()
+
+    return [
+    {
+        "id": contact[0],
+        "company_id": contact[1],
+        "name": contact[2],
+        "email": contact[3]
+    }
+    for contact in contacts
+]
 
 
 @router.post("", response_model=CompanyResponse, status_code=201)
